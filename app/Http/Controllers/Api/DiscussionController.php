bcb1dfae313856e04d11d2160f328ba6c093bace
IngeniousCreation <ingeniouscreations8@gmail.com>
2026-02-28 00:02:05 +0500

    feat: Reduce discussion pagination limit from 30 to 10
    - Changed default limit from 30 to 10 discussions per page
    - Faster initial page loads
    - More frequent but smaller data transfers

--- app/Http/Controllers/Api/DiscussionController.php
+++ app/Http/Controllers/Api/DiscussionController.php
@@ -47,7 +47,7 @@ class DiscussionController extends Controller
         }
 
         // Get pagination parameters
-        $limit = $request->input('limit', 30);
+        $limit = $request->input('limit', 10);
         $offset = $request->input('offset', 0);
 
         // Get total count of top-level discussions
